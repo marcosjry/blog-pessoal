aaa608e7039199b37c3f56743eb0d8864375c5bb
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-04-04 14:20:47 -0300

    Refactor: adicionado controller do usuário e melhoria na leitura do cod

--- src/main/java/com/blog/pessoal/acelera/maker/controller/UserController.java
+++ src/main/java/com/blog/pessoal/acelera/maker/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.blog.pessoal.acelera.maker.controller;
 
 import com.blog.pessoal.acelera.maker.DTO.usuario.UsuarioDTO;
+import com.blog.pessoal.acelera.maker.DTO.usuario.UsuarioUpdateDTO;
+import com.blog.pessoal.acelera.maker.exception.IntegridadeVioladaException;
+import com.blog.pessoal.acelera.maker.exception.UsuarioJaExisteException;
 import com.blog.pessoal.acelera.maker.model.Resposta;
 import com.blog.pessoal.acelera.maker.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,39 +21,21 @@ public class UserController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<String> criaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+    public ResponseEntity<String> criaUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) throws UsuarioJaExisteException {
         Resposta resposta = usuarioService.realizarCadastro(usuarioDTO);
-        return switch (resposta.getStatus()) {
-            case "success" -> ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
-            case "conflict" -> ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(resposta.getMensagem());
-            default ->
-                    ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Entre em contato com os desenvolvedores.");
-        };
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> atualizaDadosUsuario(@PathVariable Integer id, @RequestBody UsuarioDTO usuarioDTO){
+    public ResponseEntity<String> atualizaDadosUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioUpdateDTO usuarioDTO) throws UsuarioJaExisteException {
         Resposta resposta = usuarioService.realizaAtualizacao(id, usuarioDTO);
-        return switch (resposta.getStatus()) {
-            case "success" -> ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
-            case "conflict" -> ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(resposta.getMensagem());
-            case "error" -> ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(resposta.getMensagem());
-            case "not found" -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(resposta.getMensagem());
-            default ->
-                    ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Entre em contato com os desenvolvedores.");
-        };
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deletaUsuario(@PathVariable Integer id){
+    public ResponseEntity<String> deletaUsuario(@PathVariable Long id){
         Resposta resposta = usuarioService.realizaDelete(id);
-        return switch (resposta.getStatus()) {
-            case "success" -> ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
-            case "error" -> ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(resposta.getMensagem());
-            case "not found" -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(resposta.getMensagem());
-            default ->
-                    ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Entre em contato com os desenvolvedores.");
-        };
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(resposta.getMensagem());
     }
 
     @GetMapping
---
+++ src/main/java/com/blog/pessoal/acelera/maker/exception/UsuarioJaExisteException.java
@@ -0,0 +1,7 @@
+package com.blog.pessoal.acelera.maker.exception;
+
+public class UsuarioJaExisteException extends Exception {
+    public UsuarioJaExisteException(String message) {
+        super(message);
+    }
+}
