8de90bdfde88996e08d0ba9850f7fd47e8380414
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-04-07 20:16:17 -0300

    feat: adicionando doc SWAGGER e corrigindo erro de json loop

---
+++ src/main/java/com/blog/pessoal/acelera/maker/DTO/tema/TemaDTOResponse.java
@@ -0,0 +1,4 @@
+package com.blog.pessoal.acelera.maker.DTO.tema;
+
+public record TemaDTOResponse(Long id, String descricao) {
+}
--- src/main/java/com/blog/pessoal/acelera/maker/controller/TemaController.java
+++ src/main/java/com/blog/pessoal/acelera/maker/controller/TemaController.java
@@ -1,48 +1,159 @@
 package com.blog.pessoal.acelera.maker.controller;
 
 import com.blog.pessoal.acelera.maker.DTO.tema.TemaDTO;
+import com.blog.pessoal.acelera.maker.DTO.tema.TemaDTOResponse;
 import com.blog.pessoal.acelera.maker.exception.TemaExisteException;
 import com.blog.pessoal.acelera.maker.model.Resposta;
 import com.blog.pessoal.acelera.maker.model.Tema;
 import com.blog.pessoal.acelera.maker.service.TemaService;
+import com.blog.pessoal.acelera.maker.util.FormataRespostaGenerics;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Tag(name = "Temas", description = "Endpoints relacionados à criação, listagem, atualização e exclusão de temas.")
 @RequestMapping(path = "/api/temas")
 public class TemaController {
 
     @Autowired
     private TemaService temaService;
 
-    @PostMapping
+    @Operation(
+            summary = "Cria tema",
+            description = "Cria um novo tema",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<String> criarPostagem(@Valid @RequestBody TemaDTO temaDTO)
             throws TemaExisteException, RuntimeException {
         Resposta resposta = temaService.criarTema(temaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
-
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<String> atualizaTema(@PathVariable Long id,@Valid @RequestBody TemaDTO temaDTO) throws TemaExisteException {
-        Resposta resposta = temaService.atualiza(id ,temaDTO);
+    @Operation(
+            summary = "Atualiza um tema existente",
+            description = "Atualiza os dados de um tema com base no ID",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Tema atualizado com sucesso",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class, example = "Tema atualizado com sucesso")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Tema não encontrado",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Object.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Dados inválidos",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Object.class)
+                    )
+            )
+    })
+    @PutMapping(
+            path = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> atualizaTema(
+            @PathVariable Long id,
+            @Valid @RequestBody TemaDTO temaDTO
+    ) throws TemaExisteException {
+        Resposta resposta = temaService.atualiza(id, temaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(resposta.getMensagem());
     }
 
-    @DeleteMapping("/{id}")
+    /**
+     * Remove um tema com base no ID
+     * @param id Identificador do tema a ser removido
+     * @return Mensagem de sucesso
+     */
+    @Operation(
+            summary = "Remove um tema",
+            description = "Remove um tema com base no ID fornecido",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "202",
+                    description = "Tema removido com sucesso",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class, example = "Tema removido com sucesso")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Tema não encontrado",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Object.class)
+                    )
+            )
+    })
+    @DeleteMapping(
+            path = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<String> removeTema(@PathVariable Long id) {
         Resposta resposta = temaService.realizaDelete(id);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(resposta.getMensagem());
     }
 
-    @GetMapping
-    public ResponseEntity<List<Tema>> listaTemas() {
-        return ResponseEntity.ok(temaService.buscaTodosTemas());
+    @Operation(
+            summary = "Lista todos os temas",
+            description = "Retorna uma lista com todos os temas cadastrados",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Temas listados com sucesso",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = Tema.class))
+                    )
+            )
+    })
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<TemaDTOResponse>> listaTemas() {
+        List<Tema> temas = temaService.buscaTodosTemas();
+        return ResponseEntity.ok(
+                FormataRespostaGenerics.retornaListaFormatada(
+                temas,
+                t -> new TemaDTOResponse(t.getId(),
+                        t.getDescricao()
+                )
+        ));
     }
-
 }
