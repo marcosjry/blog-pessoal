ff23ee799b415e37eff1845ffa7e8e5997b7ed14
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-05 14:25:37 -0300

    feat: adicionado novo controller para alimentar gráfico no front

--- src/main/java/com/blog/pessoal/acelera/maker/repository/PostagemRepository.java
+++ src/main/java/com/blog/pessoal/acelera/maker/repository/PostagemRepository.java
@@ -1,11 +1,26 @@
 package com.blog.pessoal.acelera.maker.repository;
 
+import com.blog.pessoal.acelera.maker.DTO.postagem.PostStatsByUserDTO;
+import com.blog.pessoal.acelera.maker.DTO.postagem.PostsByDate;
 import com.blog.pessoal.acelera.maker.model.Postagem;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>, JpaSpecificationExecutor<Postagem> {
+
+    @Query("SELECT new com.blog.pessoal.acelera.maker.DTO.postagem.PostStatsByUserDTO(p.usuario.nome, COUNT(p)) FROM Postagem p GROUP BY p.usuario.nome")
+    List<PostStatsByUserDTO> countPostsPerUser();
+
+    @Query("SELECT new com.blog.pessoal.acelera.maker.DTO.postagem.PostsByDate(p.data, COUNT(p)) FROM Postagem p GROUP BY p.data")
+    List<PostsByDate> lastPostsByQuery(Pageable pageable);
+
+    @Query("SELECT COUNT(p) FROM Postagem p")
+    Long countNumOfPosts();
 
 }
